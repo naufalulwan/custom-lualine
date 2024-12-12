bc511654754a07cf5c40271a7c00e0018f09962c
rdl-agen46 <naufalulwan63@gmail.com>
2024-12-12 11:26:07 +0700

    new learn nvim

---
+++ lua/auto-lualine/icons.lua
@@ -0,0 +1,17 @@
+return {
+  Tree = "",
+  -- Tree = "󰉦",
+  Copilot = "",
+  CopilotOff = "",
+  Gear = "",
+  BoldError = "",
+  BoldWarning = "",
+  LineAdded = "",
+  LineModified = "",
+  LineRemoved = "",
+  FileDeleted = "",
+  Tab = "󰌒",
+  Neovim = "",
+  Branch3 = "󰘬",
+  NoBranch = "󱓌",
+}
